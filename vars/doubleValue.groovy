91e6baae8619b9105fbe8ad4ff171e3cf67b6871
md-sajidkhan <46552896+md-sajidkhan@users.noreply.github.com>
2024-08-23 19:03:20 +0530

    Update doubleValue.groovy

--- vars/doubleValue.groovy
+++ vars/doubleValue.groovy
@@ -1,6 +1,6 @@
 def call(int num){
   def doubleObj = new com.test.Double()
   def doubledValue = doubleObj.multiplyByTwo(num)
-  echo "The double value of {$num} is ${$doubledValue}"
+  echo "The double value of ${num} is ${doubledValue}"
   
 }
